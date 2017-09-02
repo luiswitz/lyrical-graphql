e78f0356b9f8545cf3c6271a5d14749fb96ec112
Luis Henrique Witz <luiswitz@gmail.com>
2017-09-02 18:29:54 -0300

    Redirect user to songs list after create a new song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router'; 
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -13,15 +15,15 @@ class SongCreate extends Component {
     event.preventDefault();
 
     this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    });
+      variables: { title: this.state.title },
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'));
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
